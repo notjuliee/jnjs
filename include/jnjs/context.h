54828ac4c7c5d59320a5a3d31219ee8f70f092ef
Julia Ahopelto <me@notjulie.dev>
2025-09-15 22:26:12 -0700

    Add context API for runtime classes

--- include/jnjs/context.h
+++ include/jnjs/context.h
@@ -24,8 +24,9 @@ class context : detail::impl_ptr<JSContext> {
     ~context() = default;
 
     value eval(std::string_view code) {
-        auto v = JS_Eval(get(), code.data(), code.size(), "<eval>", JS_EVAL_TYPE_GLOBAL | JS_EVAL_FLAG_STRICT);
-        return value(v, get());
+        auto ctx = get();
+        auto v = JS_Eval(ctx, code.data(), code.size(), "<eval>", JS_EVAL_TYPE_GLOBAL | JS_EVAL_FLAG_STRICT);
+        return value(v, ctx);
     }
 
     template <typename T> void set_global(const char *name, const T &v) {
@@ -42,6 +43,8 @@ class context : detail::impl_ptr<JSContext> {
         return _decl_class_impl(binder._d, detail::internal_class_meta<K>::data);
     }
 
+    template <typename K> void install_opaque_class() { _decl_rt_class_impl(detail::internal_class_meta<K>::data); }
+
   private:
     explicit context(JSRuntime &rt) : base(JS_NewContext(&rt), JS_FreeContext) {}
     void _set_global(const char *name, JSValue v) {
@@ -51,6 +54,7 @@ class context : detail::impl_ptr<JSContext> {
         JS_FreeValue(ctx, g);
     }
     value _make_cfunc_value(const char *name, JSCFunction *fn, int len) const;
+    void _decl_rt_class_impl(detail::internal_class_meta_data &o) const;
     void _decl_class_impl(const detail::class_builder_data &, detail::internal_class_meta_data &o);
     friend runtime;
 };
